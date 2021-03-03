b2255f75b22ebdc811c5d263c119336b20cc1f08
Sam Becerra <42310747+Zamiko@users.noreply.github.com>
2021-03-03 14:20:15 -0800

    creation of 2 dataframes
    - 'gdp' dataframe has been created and preprocessed. It only contains the countries that are included in the who covid dataset and the gdp dataset.
    - 'Qcovid' dataframe contains the number of cases per quarter for each country that are contained in both datasets.
    
    starting cases variable is the starting number of cases per quarter. (ie Q1 starts 1-1-2020.)
    
    ending cases varibale is the number of cases at the end of the quarter(ie. Q1 ends 3-31-2020.)
    
    total cases variable is the number of new cases at that partcular quarter. (aka the difference of ending cases and starting cases)
    
    same rational goes for the death variables

--- Code/Script.R
+++ Code/Script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+original_covid_data <- read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv") #DO NOT MODIFY  THIS VARIABLE
 covid <- read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
 gdp <- read.csv(file = 'gdp.csv', header = FALSE)
 
@@ -68,3 +69,116 @@ for (country in not_found){
   }
 }
 
+
+#Change the names of gdp coutries to match the covid countries dataset
+i = which(gdp$Countries == 'Bolivia')
+gdp$Countries[i] = "Bolivia (Plurinational State of)"
+
+i = which(gdp$Countries == 'Brunei')
+gdp$Countries[i] = "Brunei Darussalam"
+
+i = which(gdp$Countries == 'Iran')
+gdp$Countries[i] = "Iran (Islamic Republic of)"
+
+i = which(gdp$Countries == 'Moldova')
+gdp$Countries[i] = "Republic of Moldova"
+
+i = which(gdp$Countries == 'Palestine')
+gdp$Countries[i] = "occupied Palestinian territory, including east Jerusalem"
+
+i = which(gdp$Countries == 'Russia')
+gdp$Countries[i] = "Russian Federation"
+
+i = which(gdp$Countries == 'Tanzania')
+gdp$Countries[i] = "United Republic of Tanzania"
+
+i = which(gdp$Countries == 'United Kingdom')
+gdp$Countries[i] = "The United Kingdom"
+
+i = which(gdp$Countries == 'Vietnam')
+gdp$Countries[i] = "Viet Nam"
+
+i = which(gdp$Countries == 'USA')
+gdp$Countries[i] = "United States of America"
+
+####Other names have to "other" names and this other name can be found on google and mapped to the covid dataset 
+i = which(gdp$Countries == 'Burma (Myanmar)')
+gdp$Countries[i] = "Myanmar"
+
+i = which(gdp$Countries == 'Cape Verde')
+gdp$Countries[i] = "Cabo Verde"
+
+i = which(gdp$Countries == 'South Korea')
+gdp$Countries[i] = "Republic of Korea"
+
+
+
+
+#Sanity check  
+gdp.distinct.country = gdp %>% distinct(Countries)
+covid.distinct.country = covid %>% distinct(Country)
+
+indexes2 = gdp.distinct.country$Countries %in% covid.distinct.country$Country
+not_found2 = gdp.distinct.country$Countries[!indexes2]
+cat("From all the countries in the gdp dataset only these were not found in the covid dataset: ",not_found2,"\n")
+
+#FILTER THE COUNTRIES IN GDP THAT ALSO BELONG TO COVID DATASET
+gdp = gdp %>% filter(Countries != not_found2)
+gdp = gdp[-c(which(gdp$Countries == "Macao")),] #Had to remove manually
+
+#Get rows of interest from covid dataset 
+row_index = covid$Country %in% gdp$Countries
+covid = covid[row_index,]
+
+
+##############################################################
+covid <- covid %>% arrange(Date_reported)
+
+
+get.quarter.info = function(covid){
+  covid_quarter = tibble(quarter = "dmmy", country = "dmmy", starting_cases = -1, ending_cases = -1, total_cases = -1,
+                         starting_deaths = -1, ending_deaths = -1, total_deaths = -1)
+  
+  all_countries = covid %>% distinct(Country)
+  for (country_name in all_countries){
+    one_country <- covid %>% filter(Country == country_name)
+    Q1 = subset(one_country,Date_reported >= "2020-01-01" & Date_reported <= "2020-03-31")
+    Q2 = subset(one_country,Date_reported >= "2020-04-01" & Date_reported <= "2020-06-30")
+    Q3 = subset(one_country,Date_reported >= "2020-07-01" & Date_reported <= "2020-09-30")
+    Q4 = subset(one_country,Date_reported >= "2020-10-01" & Date_reported <= "2020-12-31")
+    
+    Q1min =  Q1 %>% filter(Date_reported == min(Q1$Date_reported))
+    Q1max =  Q1 %>% filter(Date_reported == max(Q1$Date_reported))
+    
+    Q2min =  Q2 %>% filter(Date_reported == min(Q2$Date_reported))
+    Q2max =  Q2 %>% filter(Date_reported == max(Q2$Date_reported))
+    
+    Q3min =  Q3 %>% filter(Date_reported == min(Q3$Date_reported))
+    Q3max =  Q3 %>% filter(Date_reported == max(Q3$Date_reported))
+    
+    Q4min =  Q4 %>% filter(Date_reported == min(Q4$Date_reported))
+    Q4max =  Q4 %>% filter(Date_reported == max(Q4$Date_reported))
+    
+    covid_quarter = covid_quarter %>% add_row(quarter="Q1 / 2020", country = country_name,
+                                              starting_cases= Q1min$Cumulative_cases, ending_cases = Q1max$Cumulative_cases, total_cases = Q1max$Cumulative_cases - Q1min$Cumulative_cases,
+                                              starting_deaths= Q1min$Cumulative_deaths, ending_deaths = Q1max$Cumulative_deaths, total_deaths = Q1max$Cumulative_deaths - Q1min$Cumulative_deaths)
+    
+    covid_quarter = covid_quarter %>% add_row(quarter="Q2 / 2020", country = country_name,
+                                              starting_cases= Q2min$Cumulative_cases, ending_cases = Q2max$Cumulative_cases, total_cases = Q2max$Cumulative_cases - Q2min$Cumulative_cases,
+                                              starting_deaths= Q2min$Cumulative_deaths, ending_deaths = Q2max$Cumulative_deaths, total_deaths = Q2max$Cumulative_deaths - Q2min$Cumulative_deaths)
+    
+    covid_quarter = covid_quarter %>% add_row(quarter="Q3 / 2020", country = country_name,
+                                              starting_cases= Q3min$Cumulative_cases, ending_cases = Q3max$Cumulative_cases, total_cases = Q3max$Cumulative_cases - Q3min$Cumulative_cases,
+                                              starting_deaths= Q3min$Cumulative_deaths, ending_deaths = Q3max$Cumulative_deaths, total_deaths = Q3max$Cumulative_deaths - Q3min$Cumulative_deaths)
+    
+    covid_quarter = covid_quarter %>% add_row(quarter="Q4 / 2020", country = country_name,
+                                              starting_cases= Q4min$Cumulative_cases, ending_cases = Q4max$Cumulative_cases, total_cases = Q4max$Cumulative_cases - Q4min$Cumulative_cases,
+                                              starting_deaths= Q4min$Cumulative_deaths, ending_deaths = Q4max$Cumulative_deaths, total_deaths = Q4max$Cumulative_deaths - Q4min$Cumulative_deaths)
+  }
+  
+  return(covid_quarter[-1,]) #Return all but first row because that was a dummy row
+}
+
+Qcovid = get.quarter.info(covid = covid)
+
+######## USE 'gdp' AND 'Qcovid' DATAFRAMES 
